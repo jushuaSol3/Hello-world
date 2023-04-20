0a34b0852f03b6d1da92994e880c539591417721
jushuaSol3 <127354648+jushuaSol3@users.noreply.github.com>
2023-04-20 22:48:42 +0800

    Create hello gitmate.cpp
    trial

---
+++ hello gitmate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/* my first trial of coding of hello world here*/
+//hope you support me
+
+void helloGitmate()
+{ 
+  cout << " hello gitmate " << endl;
+}
+
+
+int main(){
+
+helloGitmate();
+
+return 0;
+/* this is how i say hello mate here hehe*/
+}
+
